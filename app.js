f7c32b405ecf32b70c071f62dd50d577725f5038
desertedClub <kordelia777@gmail.com>
2025-03-25 23:03:00 +0200

    Created basic layout of the game and keyboard letters. Did CSS for the side bar custom radio buttons. Coded simple logic for an example word.

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 const sidebar = document.getElementById('sidebar')
+const keyboardEN = document.getElementById('keyboard-en')
+const buttons = keyboardEN.querySelectorAll('button')
+const wordDisplay = document.getElementById('word-display')
+
+let word = 'EXAMPLE'
+let displayWord = ""
 
 function toggleSidebar() {
     sidebar.classList.toggle('show')
+}
+
+function initializeDisplay() {
+    displayWord = "_".repeat(word.length)
+    wordDisplay.textContent = displayWord
+}
+
+initializeDisplay()
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const letter = button.dataset.letter
+        handleGuess(letter)
+
+        button.classList.add('disabled')
+        button.disabled = true
+    })
+})
+
+function handleGuess(letter) {
+    let newDisplay = ""
+    let letterFound = false
+
+    for(let i=0; i<word.length; i++) {
+        if(word[i].toUpperCase() === letter.toUpperCase()) {
+            newDisplay += letter.toUpperCase()
+            letterFound = true
+        } else newDisplay += displayWord[i]
+    }
+
+    displayWord = newDisplay
+    wordDisplay.textContent = displayWord
+
+    if(letterFound === false) console.log('incorrect guess')
+        if(!displayWord.includes('_')) console.log('you win')
 }
